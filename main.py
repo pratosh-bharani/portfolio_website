b402d23cc2421e7bb4a09230798171c55049cc56
pratoshbharani <pratosh.bharanidharan@gmail.com>
2022-12-23 07:39:08 -0500

    Added descriptions, images, and links

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ with col2:
     st.info(content)
 st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 # Import the dataset
 df = pd.read_csv("data.csv", sep=";")
@@ -24,7 +24,21 @@ df = pd.read_csv("data.csv", sep=";")
 with col3:
     for ind in df[:10].index:
         st.header(df['title'][ind])
+        st.write(df['description'][ind])
+        img_path = f"images/{df['image'][ind]}"
+        st.image(img_path)
+
+        #Below is the syntax for adding hyperlink
+        st.write(f"[Source Code]({df['url'][ind]})")
 
 with col4:
     for ind in df[10:].index:
         st.header(df['title'][ind])
+        st.write(df['description'][ind])
+        img_path = f"images/{df['image'][ind]}"
+        st.image(img_path)
+
+        # Below is the syntax for adding hyperlink
+        st.write(f"[Source Code]({df['url'][ind]})")
+
+df.columns
