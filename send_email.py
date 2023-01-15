b0130b5fbeec2d698aab4057caa5bb30f38676de
pratoshbharani <pratosh.bharanidharan@gmail.com>
2023-01-14 20:19:10 -0500

    Configured using env variables for email password

--- send_email.py
+++ send_email.py
@@ -1,12 +1,16 @@
 import smtplib, ssl
+import os
+import decouple
 
+pw = decouple.config('PASSWORD')
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "pratoshb.othersubs@gmail.com"
-    password = "celsgdifitzyldwi"
+    password = pw
+    #password = "celsgdifitzyldwi"
 
     receiver = "pratoshb.social@gmail.com"
     context = ssl.create_default_context()
@@ -14,5 +18,3 @@ def send_email(message):
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
-
-
